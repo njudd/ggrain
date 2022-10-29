b646f41bd1c032d434216394fd36ff5e99153668
Nicholas Judd <nickkjudd@gmail.com>
2022-10-29 15:21:08 +0200

    major update: added flanking, changed to geom_boxplot, cov arg made into an if/else

--- geom_rain.R
+++ geom_rain.R
@@ -18,6 +18,17 @@
 #############################################################
 # TO DO Major things denoted with *
 
+# make a covariate plot
+# remap color arg to the covariate, see if it works with categorical vars
+
+# https://github.com/tidyverse/ggplot2/blob/6434c1e1181ab8feff40e86d7cbdcd0b11cf3758/R/layer.r
+
+
+# what do they mean by works with LIKERT
+
+
+##### change geom_half_boxplot to geom_boxplot
+
 
 # need a flanking geom_rainpaired() fucntion
 
@@ -88,21 +99,17 @@
 # library(ggpp) # for position_jitternudge
 
 
+
 geom_rain <- function(mapping = NULL,
                       data = NULL,
                       trim = TRUE,
                       show.legend = NA,
                       inherit.aes = TRUE,
-                      id.long.var = NULL,
-                      
-                      rain.side = NULL,
-                      rain.center = NULL,
-                      rain.width = NULL,
+                      id.long.var = NULL, # should lines be drawn & what should connect them?
+                      cov = NULL, # should dots be colored due to a covariate?
+                      rain.side = NULL, # do you want the rainclouds (l)eft, (r)ight or (f)lanking
+                      # rain.center = NULL, currently not implimented
                       ...,
-                      # one con of doing it this way is when the user changes one it wipes them all out
-                      # yet its okay they can look at the function & copy the args
-                      # one issue is that we will have to interactively edit it for additional features (i.e., orientation)
-                      # Jordy: look at best settings for position_jitter()
                       point.args = rlang::list2(
                         ...
                       ),
@@ -123,14 +130,13 @@ geom_rain <- function(mapping = NULL,
                           seed = 42),
                       ),
                       boxplot.args =  rlang::list2(
-                        center = TRUE, errorbar.draw = FALSE, outlier.shape = NA, 
+                        outlier.shape = NA, 
                         ...
                       ),                      
                       boxplot.args.pos =  rlang::list2(
-                        side = "r", width = .08,
-                        position = position_nudge(x = .08),
+                        width = .05,
+                        position = position_nudge(x = .10),
                       ),
-                      
                       violin.args = rlang::list2( # color = NA, this default is bad incase they use group = x, or color = x
                         ...
                       ),
@@ -141,34 +147,65 @@ geom_rain <- function(mapping = NULL,
 )
 {
   
+  
+  # if rain.side == "paired" check if the defaults are used; 
+  # if so rewrite to paired nudging args; else take their args
+  
+  
   # rigth/left arguement
   # orient argument (sets what is 0)
   # rain width argument (needs to use normal width/jittersize & nudging)
   
   # the width arguement might not make sense, since its always to the scale of the plot
   
+  
+  # doing positional changes based off user input
+  if (!is.null(rain.side) && rain.side %in% c("r", "l")) {
+    violin.args.pos$side <- rain.side
+
+    if(rain.side == "l"){
+      violin.args.pos$position$x <- -violin.args.pos$position$x
+      boxplot.args.pos$position$x <- -boxplot.args.pos$position$x
+    }
+    
+  } else if (!is.null(rain.side) && rain.side %in% c("f", "flanking")){
+    
+    if ("side" %in% names(violin.args.pos)){
+      print("Warning: Option 'flanking' is used with defaults violin position arguments (i.e., violin.args.pos) \n
+            therefore new flanking position defaults are being used. If you wish to set your own defaults make sure you do \n
+            not specify a side argument for violin.args.pos")
+      
+      boxplot.args.pos <- rlang::list2(
+        width = .08,
+        position = ggpp::position_dodgenudge(x = c(-.1, -.1, .1, .1)))
+      violin.args.pos <- rlang::list2(
+        width = .7,
+        position = position_nudge(x = c(rep(-.15, 256*2), rep(-.15, 256*2),
+                                        rep(.15, 256*2), rep(.15, 256*2))))
+    }
+  } else if (!is.null(rain.side)) {
+    stop("ERROR: the rain.side arguement only accepts 'l' for left and 'r' for right \n STOPPING")
+  }
+  # combining args with position args
   point.args <- c(point.args, point.args.pos)
   line.args <- c(line.args, line.args.pos)
   boxplot.args <- c(boxplot.args, boxplot.args.pos)
   violin.args <- c(violin.args, violin.args.pos)
-  
-  if (!is.null(rain.side) && rain.side %in% c("r", "l")) {
-    violin.args$side <- rain.side
-    boxplot.args$side <- rain.side
 
-    if(rain.side == "l"){
-      violin.args$position$x <- -violin.args$position$x
-      boxplot.args$position$x <- -boxplot.args$position$x
-    }
-    
-  } else if (!is.null(rain.side)) {
-    stop("ERROR: the rain.side arguement only accepts 'l' for left and 'r' for right \n STOPPING")
+  if(!is.null(cov)){ # remap the color in e1; if a covariate is specified
+    e1 <- rlang::exec(geom_point_sorted, aes(color = !!rlang::sym(cov)), inherit.aes = TRUE, !!!point.args) # bang, bang, bang
+  }else{
+    e1 <- rlang::exec(geom_point_sorted, inherit.aes = TRUE, !!!point.args) # bang, bang, bang
   }
   
-  e1 <- rlang::exec(geom_point_sorted, inherit.aes = TRUE, !!!point.args) # bang, bang, bang
-  e3 <- rlang::exec(gghalves::geom_half_boxplot, inherit.aes = TRUE, !!!boxplot.args)
-  e4 <- rlang::exec(gghalves::geom_half_violin, inherit.aes = TRUE, !!!violin.args)
+  e3 <- rlang::exec(geom_boxplot, inherit.aes = TRUE, !!!boxplot.args)
   
+  if(!is.null(rain.side) && rain.side %in% c("f", "flanking")){
+    
+    e4 <- rlang::exec(geom_paired_raincloud,inherit.aes = TRUE, !!!violin.args)
+  }else{
+    e4 <- rlang::exec(gghalves::geom_half_violin, inherit.aes = TRUE, !!!violin.args)
+  }
   
   if (!is.null(id.long.var)){
     # quo_id.long.var = rlang::sym(id.long.var)
@@ -185,12 +222,27 @@ geom_rain <- function(mapping = NULL,
     # also the args are quite verbose, can you trim them down
     # CHECK OUT JITTER_NUDGE()
     
-    e1 <- rlang::exec(geom_point_sorted, aes(group = !!rlang::sym(id.long.var)), inherit.aes = TRUE, !!!point.args) # bang, bang, bang
-    
+    if(!is.null(cov)){ # remap the color in e1 to the covariate
+      e1 <- rlang::exec(geom_point_sorted, aes(group = !!rlang::sym(id.long.var), color = !!rlang::sym(cov)), inherit.aes = TRUE, !!!point.args) # bang, bang, bang
+    }else{
+      e1 <- rlang::exec(geom_point_sorted, aes(group = !!rlang::sym(id.long.var)), inherit.aes = TRUE, !!!point.args) # bang, bang, bang
+    }
+
     list(e2, e4, e3, e1)
-    # list(e2, e1)
-    
+
   }else{
     list(e4, e3, e1)
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
