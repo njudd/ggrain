dd5cd275aa8dd99de7599d8a11c1f69dce2375bc
Nicholas Judd <nickkjudd@gmail.com>
2025-11-14 12:52:28 -0800

    removing exampels as they were conflicting as I won't export, also fixed quantiles

--- R/geom_half_violin.R
+++ R/geom_half_violin.R
@@ -14,39 +14,17 @@
 #' @param side The side on which to draw the half violin plot. "l" for left, "r" for right, defaults to "l".
 #' @param nudge Add space between the violinplot and the middle of the space allotted to a given factor on the x-axis.
 #' @importFrom ggplot2 layer
-#' @examples
-#' ggplot(iris, aes(x = Species, y = Petal.Width, fill = Species)) +
-#'   geom_half_violin()
-#'
-#' ggplot(iris, aes(x = Species, y = Petal.Width, fill = Species)) +
-#'   geom_half_violin(side = "r")
-#'   
-#' ggplot() +
-#'   geom_half_violin(
-#'     data = ToothGrowth, 
-#'     aes(x = as.factor(dose), y = len, split = supp, fill = supp),
-#'     position = "identity"
-#'   ) + 
-#'   theme_minimal()  
-#'   
-#' ggplot(ToothGrowth, aes(x = supp, y = len, color = supp)) + 
-#'   geom_half_violin(side = c("l", "r"))
-#' 
-#' ggplot(data = ToothGrowth, aes(x = 1, y = len)) +
-#'   geom_half_point(aes(y = len), side = "l") +
-#'   geom_half_violin(aes(y = len), side = "r") +
-#'   coord_flip()
-#' 
-#' @export
+#' @keywords internal
 #' @references Hintze, J. L., Nelson, R. D. (1998) Violin Plots: A Box
 #' Plot-Density Trace Synergism. The American Statistician 52, 181-184.
+
 geom_half_violin <- function(
   mapping = NULL, data = NULL,
   stat = "half_ydensity", position = "dodge",
   ...,
   side = "l",
   nudge = 0,
-  draw_quantiles = NULL,
+  quantiles = NULL,
   trim = TRUE,
   scale = "area",
   na.rm = FALSE,
@@ -65,18 +43,17 @@ geom_half_violin <- function(
       nudge = nudge,
       trim = trim,
       scale = scale,
-      draw_quantiles = draw_quantiles,
+      quantiles = quantiles,
       na.rm = na.rm,
       ...
     )
   )
 }
 
-#' @rdname gghalves-extensions
 #' @format NULL
 #' @usage NULL
 #' @importFrom ggplot2 ggproto GeomViolin GeomBoxplot GeomPolygon
-#' @export
+#' @keywords internal
 GeomHalfViolin <- ggproto(
   "GeomHalfViolin", GeomViolin,
   
@@ -100,7 +77,7 @@ GeomHalfViolin <- ggproto(
     params
   },
   
-  draw_group = function(self, data, side = "l", nudge = 0, ..., draw_quantiles = NULL) {
+  draw_group = function(self, data, side = "l", nudge = 0, ..., quantiles = NULL) {
     # Find the points for the line to go all the way around
     is_panel <- data$group[1] == -1
     is_group <- FALSE
@@ -133,11 +110,11 @@ GeomHalfViolin <- ggproto(
     newdata <- rbind(newdata, newdata[1,])
 
     # Draw quantiles if requested, so long as there is non-zero y range
-    if (length(draw_quantiles) > 0 & !scales::zero_range(range(data$y))) {
-      stopifnot(all(draw_quantiles >= 0), all(draw_quantiles <= 1))
+    if (!is.null(quantiles) & !scales::zero_range(range(data$y))) {
+      stopifnot(all(quantiles >= 0), all(quantiles <= 1))
 
       # Compute the quantile segments and combine with existing aesthetics
-      quantiles <- ggplot2:::create_quantile_segment_frame(data, draw_quantiles)
+      quantiles <- ggplot2:::create_quantile_segment_frame(data, quantiles)
       aesthetics <- data[
         rep(1, nrow(quantiles)),
         setdiff(names(data), c("x", "y", "group")),
@@ -152,7 +129,7 @@ GeomHalfViolin <- ggproto(
         GeomPath$draw_panel(both, ...)
       }
 
-      ggplot2:::ggname("geom_half_violin", grobTree(
+      ggplot2:::ggname("geom_half_violin", grid::grobTree(
         GeomPolygon$draw_panel(newdata, ...),
         quantile_grob)
       )
